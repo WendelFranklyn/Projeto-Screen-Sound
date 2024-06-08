66d751299d247fc53fc700a9a1698f2ef3419dc9
WendelFranklyn <wendelwendel_franklyn@hotmail.com>
2024-06-08 12:48:33 -0300

    Adiciona operação 1 e 2, adicionar bandar e verificar bandas adicionadas

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿string mensagem = "Boas vindas ao Screen Sound";
+List<string> listaBandas = new List<string>();
 
-void ExibirMensagem()
+void ExibirLogo()
 {
     //O @ no C# antes da string, é para chamar a string de uma maneira como você gostaria de ver, vulgo exemplo
     Console.WriteLine(@"
@@ -28,10 +29,10 @@ void ExibirMenu()
     switch (opcaoNumerica)
     {
         case 1:
-            Console.WriteLine("Você escolheu a opção " + opcaoNumerica);
+            RegistrarBandas();
             break;
         case 2:
-            Console.WriteLine("Você escolheu a opção " + opcaoNumerica);
+            MostrarRegistroBandas();
             break;
         case 3:
             Console.WriteLine("Você escolheu a opção " + opcaoNumerica);
@@ -47,6 +48,42 @@ void ExibirMenu()
             break;
     }
 }
-ExibirMensagem();
+void RegistrarBandas()
+{
+    Console.Clear();
+    Console.WriteLine("********************");
+    Console.WriteLine("Registro de bandas");
+    Console.WriteLine("********************\n");
+    Console.Write("Digite o nome da banda que você deseja registrar: ");
+    string nomeBanda = Console.ReadLine()!;
+    listaBandas.Add(nomeBanda);
+    Console.WriteLine($"A banda {nomeBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirLogo();
+    ExibirMenu();
+}
+
+void MostrarRegistroBandas()
+{
+    Console.Clear();
+    Console.WriteLine("*************************************");
+    Console.WriteLine("Exibindo todas as bandas registradas");
+    Console.WriteLine("*************************************\n");
+    //for (int i = 0; i < listaBandas.Count; i++)
+    //{
+    //    Console.WriteLine($"Banda: {listaBandas[i]}");
+    //}
+    foreach (string banda in listaBandas)
+    {
+        Console.WriteLine($"Banda: {banda}");
+    }
+    Console.WriteLine(\n"Digite uma tecla para voltar ao menu principal.");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirMenu();
+}
+
+ExibirLogo();
 ExibirMenu();
 Console.ReadLine();
